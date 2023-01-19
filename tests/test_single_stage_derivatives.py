036b27faa78f4695c5b380052f3512837fcad337
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-01-19 15:14:47 +0000

    Refined the test figures

--- tests/test_single_stage_derivatives.py
+++ tests/test_single_stage_derivatives.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 mpi = MpiPartition()
 
 # Define absolute steps
-abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7]
+abs_step_array = [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6]
 rel_step_value = 1e-5
 
 # Input parameters
