773cbd3e72eecd6c51ff428fc5477000451f3479
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-01-24 21:46:31 +0000

    Slightly different input parameters

--- src/inputs.py
+++ src/inputs.py
@@ -1,7 +1,7 @@
 ## Inputs Parameters overwritten with command line arguments
-create_wout_final = False # Create a VMEC wout file entitled "final"
-vmec_plot_result = False # Plot the optimized VMEC result
-booz_xform_plot_result = False # Plot the resulting field in Boozer coordinates
+create_wout_final = True # Create a VMEC wout file entitled "final"
+vmec_plot_result = True # Plot the optimized VMEC result
+booz_xform_plot_result = True # Plot the resulting field in Boozer coordinates
 # CNT -> QA stellarator with four coils
 vmec_input_start_CNT = 'input.CNT' # VMEC input file that serves as a start for the optimization when there are no previous results
 LENGTHBOUND_CNT = 4.0 # Threshold for the length of each coil
@@ -9,7 +9,7 @@ CC_THRESHOLD_CNT = 0.15 # Threshold for the coil-to-coil distance penalty in the
 CURVATURE_THRESHOLD_CNT = 11.0 # Threshold for the curvature penalty in the objective function
 MSC_THRESHOLD_CNT = 11.0 # Threshold for the mean squared curvature penalty in the objective function
 include_iota_target_CNT = True # Specify if iota should be added to the objective function
-iota_target_CNT = -0.23 # Target rotational transform iota
+iota_target_CNT = -0.33 # Target rotational transform iota
 aspect_ratio_target_CNT = 3.5  # Target aspect ratio
 nphi_CNT = 128 # Toroidal resolution for the Biot-Savart magnetic field
 quasisymmetry_helicity_n_CNT = 0 # Toroidal quasisymmetry integer N in |B|
